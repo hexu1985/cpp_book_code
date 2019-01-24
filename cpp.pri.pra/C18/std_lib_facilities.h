6f3c4789523b159fd46f47c435763d72c920b8b4
hexu <hexu_1985@sina.com>
2019-01-24 11:17:42 +0800

    add cpp.pri.pra C17 - C27

---
+++ cpp.pri.pra/C18/std_lib_facilities.h
@@ -0,0 +1,20 @@
+
+//
+// This is a standard library support code to the chapters of the book
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+//
+
+#ifndef STD_LIB_FACILITIES_GUARD
+#define STD_LIB_FACILITIES_GUARD 1
+
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//------------------------------------------------------------------------------
+
+#endif // STD_LIB_FACILITIES_GUARD
